36be89b81179fe2cdfad9ad746b4d37d6ceb9239
Luke Rhodes <rhodesy22@gmail.com>
2017-03-13 23:59:43 +1100

    Added support for relatively dumb IR thermostat control

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function RMMini3Accessory(log, config) {
   this.host = config["host"];
   this.name = config["name"];
   this.data = config["data"];
+  
+  this.targetTemperature = 0
+  this.temperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
 }
 
 RMMini3Accessory.prototype = {
@@ -45,6 +48,45 @@ RMMini3Accessory.prototype = {
       callback();
     });
   },
+  
+  // Required
+	getCurrentHeatingCoolingState: function(callback) {
+		this.log("getCurrentHeatingCoolingState (always 'auto')");
+    callback(null, Characteristic.TargetHeatingCoolingState.AUTO); // success
+	},
+	getTargetHeatingCoolingState: function(callback) {
+		this.log("getTargetHeatingCoolingState (always 'auto')";
+    
+		callback(null, Characteristic.TargetHeatingCoolingState.AUTO); // success
+	},
+	setTargetHeatingCoolingState: function(value, callback) {
+		this.log("setTargetHeatingCoolingState (do nothing");
+    callback(null);
+	},
+	getCurrentTemperature: function(callback) {
+		this.log("getCurrentTemperature (" + this.targetTemperature +")");
+		
+		callback(null, this.currentTemperature); // success
+	},
+	getTargetTemperature: function(callback) {
+		this.log("getTargetTemperature (" + this.targetTemperature +")");
+		callback(null, this.targetTemperature)
+	},
+	setTargetTemperature: function(value, callback) {
+		this.log("setTargetTemperature (" + this.targetTemperature +")");
+    this.targetTemperature = value
+		callback(null)
+	},
+	getTemperatureDisplayUnits: function(callback) {
+		this.log("getTemperatureDisplayUnits:", this.temperatureDisplayUnits);
+		var error = null;
+		callback(error, this.temperatureDisplayUnits);
+	},
+	setTemperatureDisplayUnits: function(value, callback) {
+		this.log("setTemperatureDisplayUnits from %s to %s", this.temperatureDisplayUnits, value);
+		this.temperatureDisplayUnits = value;
+		callback(null);
+	},
 
   identify: function (callback) {
     this.log("Identify requested!");
@@ -75,6 +117,35 @@ RMMini3Accessory.prototype = {
           .getCharacteristic(Characteristic.On)
           .on('set', this.setPowerState.bind(this, data["on"], data["off"]));
         services.push(switchService);
+      } else if (type == "thermostat") {
+        this.log("add thermostat service");
+        var thermostatService = new Service.Thermostat(data.name || this.name);
+        
+       	// Required Characteristics
+        thermostatService
+          .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+          .on('get', this.getCurrentHeatingCoolingState.bind(this));
+
+        thermostatService
+          .getCharacteristic(Characteristic.TargetHeatingCoolingState)
+          .on('get', this.getTargetHeatingCoolingState.bind(this))
+          .on('set', this.setTargetHeatingCoolingState.bind(this));
+
+        thermostatService
+          .getCharacteristic(Characteristic.CurrentTemperature)
+          .on('get', this.getCurrentTemperature.bind(this));
+
+        thermostatService
+          .getCharacteristic(Characteristic.TargetTemperature)
+          .on('get', this.getTargetTemperature.bind(this))
+          .on('set', this.setTargetTemperature.bind(this));
+
+        thermostatService
+          .getCharacteristic(Characteristic.TemperatureDisplayUnits)
+          .on('get', this.getTemperatureDisplayUnits.bind(this))
+          .on('set', this.setTemperatureDisplayUnits.bind(this)); 
+
+        services.push(thermostatService);
       } else if (type == "channel") {
         this.log("add channel service");
         var channels = [""];
